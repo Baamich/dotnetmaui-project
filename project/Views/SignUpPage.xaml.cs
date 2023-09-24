d3aa2a06ada10f0734b6926681a7f81a6b8dcd79
Baam <petasurucanu@gmail.com>
2023-09-24 21:51:28 +0300

    Added a button and password verification

--- project/Views/SignUpPage.xaml.cs
+++ project/Views/SignUpPage.xaml.cs
@@ -5,10 +5,19 @@ public partial class SignUpPage : ContentPage
     public SignUpPage()
     {
         InitializeComponent();
+        if (Password != Password2)
+        {
+            ErorPassword.Text = "Password does't match ";
+        }
+        else if (Password == Password2)
+        {
+            ErorPassword.Text = "";
+        }
     }
 
     private async void Button_Clicked(System.Object sender, System.EventArgs e)
     {
         await Navigation.PopModalAsync();
     }
+
 }
