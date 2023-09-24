4864c134382a9b3f6eb31308e6ab097f0b60a1c6
Kirill Yaymchuk <stbestich@gmail.com>
2023-09-24 16:00:29 +0300

    Login page

---
+++ project/Views/LogginPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace project.Views;
+
+public partial class LogginPage : ContentPage
+{
+    public LogginPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void SignUpButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Navigation.PushModalAsync(new SignUpPage());
+    }
+}
