81a302875e8b6fd9feca2a7cfdade6a67c5eb06f
Kirill Yaymchuk <stbestich@gmail.com>
2023-09-24 15:48:09 +0300

    Created Login page desing

--- project/App.xaml.cs
+++ project/App.xaml.cs
@@ -1,4 +1,11 @@
-﻿namespace project
+﻿#if __ANDROID__
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+using Android.Content.Res;
+#endif
+
+using project.Views;
+
+namespace project
 {
     public partial class App : Application
     {
@@ -6,7 +13,13 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new LogginPage();
+
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderLine", (handler, view) => {
+#if __ANDROID__
+                (handler.PlatformView as Android.Views.View).SetBackgroundColor(Microsoft.Maui.Graphics.Colors.Transparent.ToAndroid());
+#endif
+            });
         }
     }
 }
--- project/AppShell.xaml.cs
+++ project/AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace project
-{
+﻿using project.Views;
+
+namespace project
+{    
     public partial class AppShell : Shell
     {
+        const string logginPageRoutStr = "loggin";
+
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(logginPageRoutStr, typeof(LogginPage));
         }
     }
 }
