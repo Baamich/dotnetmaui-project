411c8a9e3c5808106fdd49edac4b351271d56e62
Kirill Yaymchuk <stbestich@gmail.com>
2023-09-26 17:32:09 +0300

    Created navigation after loading logic

--- project/Views/LoadingPage.xaml.cs
+++ project/Views/LoadingPage.xaml.cs
@@ -2,8 +2,30 @@
 
 public partial class LoadingPage : ContentPage
 {
+    private string secStorageKey = "hasAuth";
+
     public LoadingPage()
     {
         InitializeComponent();
+    }
+
+    protected override async void OnNavigatedTo(NavigatedToEventArgs eventArgs)
+    {
+        if (await isAuntificated())
+        {
+            await Shell.Current.GoToAsync("loggin"); //temp value for test
+        }
+        else
+        {
+            await Shell.Current.GoToAsync("login"); //temp value for test
+        }
+        base.OnNavigatedTo(eventArgs);
+    }
+
+    private async Task<bool> isAuntificated()
+    {
+        await Task.Delay(2000);
+        var hasAuth = await SecureStorage.GetAsync(secStorageKey);
+        return !(hasAuth == null);
     }
 }
