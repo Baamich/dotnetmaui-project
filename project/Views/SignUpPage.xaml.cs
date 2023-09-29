932682f223d441d3f1360165363eba0ec96469c9
Kirill Yaymchuk <stbestich@gmail.com>
2023-09-29 18:21:57 +0300

    Fixed bug

--- project/Views/SignUpPage.xaml.cs
+++ project/Views/SignUpPage.xaml.cs
@@ -23,17 +23,22 @@ public partial class SignUpPage : ContentPage
     private bool HandleErrorLabel()
     {
         ErrorLabel.Text = null;
+        if (!EmailEntry.Text.Contains("@gmail.com"))
+        {
+            ErrorLabel.Text = "Enter email in right format.";
+            return false;
+        }
         if (PasswordEntry.Text != RepeatPasswordEntry.Text)
         {
             ErrorLabel.Text = "Password does't match.";
             return false;
         }
-        else if (PasswordEntry.Text.Length < 4)
+        if (PasswordEntry.Text.Length < 4)
         {
             ErrorLabel.Text = "Password must be longer than 4 digits.";
             return false;
         }
-        return true;
+        return false;
     }
 
     private async void ReturnButton_Clicked(System.Object sender, System.EventArgs e)
