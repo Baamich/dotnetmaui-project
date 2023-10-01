e062ad655357aeedb418e7056124eca2b9a24a55
Kirill Yaymchuk <stbestich@gmail.com>
2023-10-01 12:00:03 +0300

    Improved loading screen

--- project/Views/LoadingPage.xaml.cs
+++ project/Views/LoadingPage.xaml.cs
@@ -22,8 +22,17 @@ public partial class LoadingPage : ContentPage
 
     private async Task<bool> isAuntificated()
     {
-        await Task.Delay(2000);
+        await LoadingScreenHandler();
         var hasAuth = await SecureStorage.GetAsync(AppRoutes.secStorageKey);
         return !(hasAuth == null);
+    }
+
+    private async Task LoadingScreenHandler()
+    {
+        await Task.Delay(1000);
+        LoadingInfoText.Text = "Checking data integrity...";
+        await Task.Delay(3000);
+        LoadingInfoText.Text = "Loading data...";
+        await Task.Delay(2000);
     }
 }
